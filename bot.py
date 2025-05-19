a3f41b429af8feafecfc61ed0cc353da290ba4a8
RabitoConejito <stefanycordero97@gmail.com>
2025-05-18 20:06:26 -0400

    Fix: actualiza bot.py para compatibilidad con nueva versión de PTB

--- bot.py
+++ bot.py
@@ -753,7 +753,7 @@ async def reenviado_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # === SETUP FINAL ===
 
 print("Bot funcionando correctamente")
-app = ApplicationBuilder().token("1977028208:AAHpkAqAx78Ph5zErJWVfb9Y0wHMwNT9kzs").drop_pending_updates(True).build()
+application = ApplicationBuilder().token("1977028208:AAHpkAqAx78Ph5zErJWVfb9Y0wHMwNT9kzs").build(drop_pending_updates=True)
 
 # ✅ Configuraciones iniciales
 app.add_error_handler(error_handler)
