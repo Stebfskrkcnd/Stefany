fb70c1e59e7a2d7aa44fd9527dfbb14398e183cb
RabitoConejito <stefanycordero97@gmail.com>
2025-05-21 15:39:54 -0400

    chores: trigger manual deploy

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,12 +1,12 @@
 import json
 
+def save_json(path, data):
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=2, ensure_ascii=False)
+
 def load_json(path):
     try:
         with open(path, "r", encoding="utf-8") as f:
             return json.load(f)
     except FileNotFoundError:
         return []
-
-def save_json(path, data):
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
