056f90881309d46527c15d98129e0f575f0542fa
RabitoConejito <stefanycordero97@gmail.com>
2025-05-28 12:55:39 -0400

    Agrego config.py después de quitarlo de .gitignore

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+# Cargar token
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Cargar usuarios autorizados desde string: "12345;67890"
+USUARIOS_AUTORIZADOS = [
+    int(uid) for uid in os.getenv("USUARIOS_AUTORIZADOS", "").split(";") if uid.strip().isdigit()
+]
+
+# Cargar canales fijos desde string JSON (idealmente set en Railway como texto plano)
+try:
+    CANALES_FIJOS = json.loads(os.getenv("CANALES_FIJOS", "[]"))
+except json.JSONDecodeError:
+    CANALES_FIJOS = []
+    
+ENCABEZADO_FILEID = os.getenv("ENCABEZADO_FILEID", "")
+ENCABEZADO_CAPTION = os.getenv("ENCABEZADO_CAPTION", "")
+
+# Zona horaria
+ZONA_HORARIA = os.getenv("ZONA_HORARIA", "America/New_York")
